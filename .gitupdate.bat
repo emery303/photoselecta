9d315c8dfd42a69e4ce56dd8887bd2c645f6a58b
Vincze Imre <emery303@gmail.com>
2014-03-25 10:15:59 +0100

    automatic update

---
+++ .gitupdate.bat
@@ -0,0 +1,7 @@
+@echo off
+git bash
+git remote add origin https://github.com/emery303/photoselecta
+git add -A
+git commit -m "automatic update"
+git push -f origin master
+git pull https://github.com/emery303/photoselecta
